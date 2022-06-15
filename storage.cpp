9f7d40167d95224735c0c9475c7a01b9121ef7ae
Bofeng Pan <nog3952149@outlook.com>
2022-06-15 23:42:53 +0800

    增加delete

--- storage.cpp
+++ storage.cpp
@@ -1,3 +1,9 @@
 #include "storage.h"
 
 vector<Graph*> Storage::graphs;
+
+Storage::~Storage()
+{
+	for (Graph* g : graphs)
+		delete g;
+}
--- storage.h
+++ storage.h
@@ -11,6 +11,7 @@ class Storage
 {
 public:
 	Storage() {}
+	~Storage();
 	static vector<Graph*> graphs;
 	queue<string> infix;  //儲存中序式
 	vector<string> postfix;  //儲存後序式
